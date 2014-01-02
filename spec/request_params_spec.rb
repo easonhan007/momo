3de28a7d55049ce4ec52191d6a8d2d8074758aaa
easonhan <nbkhic@qq.com>
2014-01-02 15:07:16 +0800

    add params test

--- spec/request_params_spec.rb
+++ spec/request_params_spec.rb
@@ -2,8 +2,33 @@ require 'spec_helper'
 
 describe 'request::params' do 
 
-	it 'should access /params with params' do
+	it 'should access /params with params and null value' do
 		get('/params',{'blah' => nil})
+		expect(last_response).to be_ok
+		expect(last_response.body).to eql('bar')
+	end
+
+	it 'should not get response text when access /params without params' do
+		get('/params')
+		expect(last_response).to be_ok
+		expect(last_response.body).not_to eql('bar')
+		expect(last_response.body).to be_empty
+	end
+
+	it 'should access /multi_params with muli params' do
+		get('/multi_params',{'foo' => nil, 'bar' => nil})
+		expect(last_response).to be_ok
+		expect(last_response.body).to eql('foo.bar')
+	end
+	
+	it 'should access /params_with_value with params and value' do
+		get('/params_with_value',{'key' => 'value'})
+		expect(last_response).to be_ok
+		expect(last_response.body).to eql('bar')
+	end
+
+	it 'should access /multi_params_with_value with params and value' do
+		get('/multi_params_with_value',{'key' => 'value', 'another' => 'value'})
 		expect(last_response).to be_ok
 		expect(last_response.body).to eql('bar')
 	end
