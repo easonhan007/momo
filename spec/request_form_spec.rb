b431e17eb83f89fa48ce29bf81126571dd469cad
easonhan <nbkhic@qq.com>
2014-01-03 22:17:01 +0800

    add form tests

---
+++ spec/request_form_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe 'request::form' do 
+
+	it 'should access /from with form params when post' do
+		post('/form',{'foo' => 'bar'})
+		expect(last_response).to be_ok
+		expect(last_response.body).to eql('form')
+	end
+
+	it 'should not get correct result when post /from without form params' do
+		post('/form')
+		expect(last_response).to be_ok
+		expect(last_response.body).not_to eql('form')
+		expect(last_response.body).to eql('')
+	end
+
+	it 'should work well when post with form and ? params' do
+		post('/form_with_params?foo=bar', {'key' => 'value'})
+		expect(last_response).to be_ok
+		expect(last_response.body).to eql('form')
+	end
+
+	def app
+		MockServer
+	end
+
+end
